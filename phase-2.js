9ea789569d5b633b081f6ec4ed768ea326fb6e20
Royce Calvin <royce20002006@yahoo.com>
2024-01-23 23:08:04 -0600

    on phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,9 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  let arr = [];
+  let steps = 0;
+  for (let i = increment; steps < 10; i += increment) {
+    result = addNums(i);
+    steps++
+    // console.log(i)
+    arr.push(result);
+    // console.log(result)
+  }
+  console.log(arr)
+  return arr;
 }
+
+console.log(addNums10(10));
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {

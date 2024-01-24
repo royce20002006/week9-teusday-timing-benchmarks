ba8bfc72ebd2d5c53d1af23567e0380d65b54788
Royce Calvin <royce20002006@yahoo.com>
2024-01-23 23:12:08 -0600

    finished 2

--- phase-2.js
+++ phase-2.js
@@ -11,7 +11,7 @@ function addNums10(increment) {
     arr.push(result);
     // console.log(result)
   }
-  console.log(arr)
+  
   return arr;
 }
 
@@ -19,7 +19,16 @@ console.log(addNums10(10));
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let arr = [];
+  let steps = 0;
+  for (let i = increment; steps < 10; i += increment) {
+    result = addManyNums(i);
+    steps++
+    arr.push(result);
+  }
+  // console.log(arr);
+  return arr;
+  
 
 }
 

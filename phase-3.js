164f51883d3be0da3fd4ea54e00a499bf4156a10
Royce Calvin <royce20002006@yahoo.com>
2024-01-23 23:29:40 -0600

    finished

--- phase-3.js
+++ phase-3.js
@@ -4,26 +4,54 @@ function addNums10Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
 
-  // Your code here 
+  let arr = [];
+  let steps = 0;
+  for (let i = increment; steps < 10; i += increment) {
+    let start = Date.now()
+    result = addNums(i);
+    let end =Date.now()
+    console.log(end - start)
+    steps++
+    // console.log(i)
+    arr.push(result);
+    // console.log(result)
+  }
+
+  return arr;
 
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+  // Copy your `addManyNums10` code here
+  // Then, add timing code
 
-  // Your code here 
+  let arr = [];
+  let steps = 0;
+  for (let i = increment; steps < 10; i += increment) {
+    
+    result = addManyNums(i);
+    
+    steps++
+    arr.push(result);
+  }
+  // console.log(arr);
+  return arr;
 
 }
 
 
 n = 1000000
 console.log(`addNums(${n}): `);
+
 addNums10Timing(1000000);
 
 console.log("\n***********\n");
 
+
+// let startT = Date.now()
 n = 1000
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
+// let endT = Date.now();
+// console.log(endT - startT)
